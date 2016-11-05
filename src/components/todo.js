c1f85fc173dac6c17e98e606038e6d5839b464ef
bertolomych <rom.bekk@gmail.com>
2016-11-06 02:02:51 +0300

    better styling & markdown

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import mdIt from 'markdown-it';
+import bem from '../lib/bem';
 import './todo.css';
 
+const createMarkup = md => ({__html: mdIt({linkify: true}).render(md)});
+
 const Todo = ({todo}) => (
-	<li className="todo">{todo.todo}</li>
+	<li className={bem('todo', {resolved: todo.resolved})}>
+		<input type="checkbox" className={bem('todo', 'resolved')} />
+		<div
+			dangerouslySetInnerHTML={createMarkup(todo.todo)}
+			className={bem('todo', 'content')} />
+		<div className={bem('todo', 'actions')}>
+			<i className={`fa fa-pencil ${bem('todo', 'action', {edit: true})}`}/>
+			<i className={`fa fa-trash ${bem('todo', 'action', {delete: true})}`}/>
+		</div>
+	</li>
 );
 
 export default Todo;

dcd3655ff96173bed6addf33c5e00cbc146204cc
bertolomych <rom.bekk@gmail.com>
2017-04-17 10:33:38 +0300

    move stuff to the model

--- src/App.js
+++ src/App.js
@@ -1,108 +1,76 @@
 import React, {Component} from 'react';
 import {DragDropContext} from 'react-dnd';
-import findIndex from 'lodash/findIndex';
 import HTML5Backend from 'react-dnd-html5-backend';
 
 import {loadFromLocalStorage, saveToLocalStorage} from './lib/persist';
-import {replace, splice} from './lib/array-utils';
 import Sheet from './components/sheet';
 import TodoList from './components/todo-list';
 import {INBOX, TODO, WEEK, ONCE} from './constants';
+import TodosModel, {getTodosByType} from './model/todos';
 import './App.css';
-
-const selectTodoIndexById = (state, id) => findIndex(
-	state.todos,
-	(todo) => todo.id === id
-);
 
 class App extends Component {
 	constructor() {
 		super();
-		this.state = loadFromLocalStorage() || {todos: []};
-		this.addItem = (type, todo) => {
-			const newTodo = {id: Date.now(), type, todo, resolved: false};
-			this.setState({todos: this.state.todos.concat(newTodo)});
-		};
-		this.toggleResolve = (id) => {
-			const index = selectTodoIndexById(this.state, id);
-			const todo = this.state.todos[index];
-			this.setState({
-				todos: replace(
-					this.state.todos,
-					index,
-					Object.assign({}, todo, {resolved: !todo.resolved})
-				)
-			});
-		};
-		this.removeItem = (id) => {
-			const index = selectTodoIndexById(this.state, id);
-			this.setState({todos: splice(this.state.todos, index, 1)});
-		};
-		this.saveItem = (id, newValue) => {
-			const index = selectTodoIndexById(this.state, id);
-			const todo = this.state.todos[index];
-			this.setState({
-				todos: replace(
-					this.state.todos,
-					index,
-					Object.assign({}, todo, {todo: newValue})
-				)
-			});
-		};
+		const oldTodos = loadFromLocalStorage() || [];
+		this.model = new TodosModel(oldTodos, ({todos}) => {
+			saveToLocalStorage(todos);
+			this.setState({todos});
+		});
+		this.state = {todos: oldTodos};
 	}
 
 	render() {
-		const inbox = this.state.todos.filter((todo) => todo.type === INBOX);
-		const todoNow = this.state.todos.filter((todo) => todo.type === TODO);
-		const week = this.state.todos.filter((todo) => todo.type === WEEK);
-		const once = this.state.todos.filter((todo) => todo.type === ONCE);
-		saveToLocalStorage(this.state); // TODO where this should be placed actually?
+		const inbox = getTodosByType(this.state.todos, INBOX);
+		const todoNow = getTodosByType(this.state.todos, TODO);
+		const week = getTodosByType(this.state.todos, WEEK);
+		const once = getTodosByType(this.state.todos, ONCE);
 
 		return (
 			<div className="App">
 				<Sheet
 					title="inbox"
 					todoType={INBOX}
-					addItem={this.addItem}
+					addItem={this.model.addItem}
 					placeholder="I have something new...">
 					<TodoList
 						todos={inbox}
-						toggleResolve={this.toggleResolve}
-						removeItem={this.removeItem}
-						saveItem={this.saveItem} />
+						toggleResolve={this.model.toggleResolve}
+						removeItem={this.model.removeItem}
+						saveItem={this.model.saveItem} />
 				</Sheet>
 				<Sheet
 					title="todo"
 					todoType={TODO}
-					addItem={this.addItem}
+					addItem={this.model.addItem}
 					placeholder="Today I have to do...">
 					<TodoList
 						todos={todoNow}
-						toggleResolve={this.toggleResolve}
-						removeItem={this.removeItem}
-						saveItem={this.saveItem} />
+						toggleResolve={this.model.toggleResolve}
+						removeItem={this.model.removeItem}
+						saveItem={this.model.saveItem} />
 				</Sheet>
 				<Sheet
 					title="week"
 					todoType={WEEK}
-					addItem={this.addItem}
+					addItem={this.model.addItem}
 					placeholder="This week I have to do...">
 					<TodoList
 						todos={week}
-						toggleResolve={this.toggleResolve}
-						removeItem={this.removeItem}
-						saveItem={this.saveItem} />
+						toggleResolve={this.model.toggleResolve}
+						removeItem={this.model.removeItem}
+						saveItem={this.model.saveItem} />
 				</Sheet>
 				<Sheet
 					title="at some point"
 					todoType={ONCE}
-					addItem={this.addItem}
+					addItem={this.model.addItem}
 					placeholder="Once I have to do...">
 					<TodoList
 						todos={once}
-						toggleResolve={this.toggleResolve}
-						removeItem={this.removeItem}
-						saveItem={this.saveItem} />
+						toggleResolve={this.model.toggleResolve}
+						removeItem={this.model.removeItem}
+						saveItem={this.model.saveItem} />
 				</Sheet>
 			</div>
 		);
---
+++ src/model/todos.js
@@ -0,0 +1,54 @@
+import findIndex from 'lodash/findIndex';
+import filter from 'lodash/filter';
+
+import {replace, splice} from '../lib/array-utils';
+
+const selectTodoIndexById = (todos, id) => findIndex(
+	todos,
+	(todo) => todo.id === id
+);
+
+export default function TodosModel(initialTodos, subscriber) {
+	let todos = initialTodos;
+
+	function notify() {
+		if (typeof subscriber === 'function') {
+			subscriber({todos});
+		}
+	}
+
+	this.addItem = (type, todo) => {
+		const newTodo = {id: Date.now(), type, todo, resolved: false};
+		todos = todos.concat(newTodo);
+		notify();
+	};
+	this.toggleResolve = (id) => {
+		const index = selectTodoIndexById(todos, id);
+		const todo = todos[index];
+		todos = replace(
+			todos,
+			index,
+			Object.assign({}, todo, {resolved: !todo.resolved})
+		);
+		notify();
+	};
+	this.removeItem = (id) => {
+		const index = selectTodoIndexById(todos, id);
+		todos = splice(todos, index, 1);
+		notify();
+	};
+	this.saveItem = (id, newValue) => {
+		const index = selectTodoIndexById(todos, id);
+		const todo = todos[index];
+		todos = replace(
+			todos,
+			index,
+			Object.assign({}, todo, {todo: newValue})
+		);
+		notify();
+	};
+}
+
+export function getTodosByType(todos, type) {
+	return filter(todos, (todo) => todo.type === type);
+}
